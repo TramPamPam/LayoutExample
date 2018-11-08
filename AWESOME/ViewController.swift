58d1c5c3688d7333dfa6eb2396febedb2dd23c24
Oleksandr Bezpalchuk <oleksandr.bezpalchuk@ekreative.com>
2018-11-08 19:07:46 +0200

    Initial Commit

---
+++ AWESOME/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AWESOME
+//
+//  Created by Oleksandr on 11/8/18.
+//  Copyright © 2018 Oleksandr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
